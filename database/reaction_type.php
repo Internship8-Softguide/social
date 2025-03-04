35397c7a4a57e214218638ffe0bc0711a3cfca56
Saw Theodore <sawtheodore@gmail.com>
2025-03-04 09:12:08 +0630

    no change

--- database/reaction_type.php
+++ database/reaction_type.php
@@ -6,6 +6,7 @@ function create_reaction_type($mysqli, $reactionName)
     try {
         $sql = "INSERT INTO `reactiontype`(`reactionName`) VALUES ('$reactionName')";
         $mysqli->query($sql);
+        
     } catch (Exception $th) {
         echo "Can not create reaction Type";
     }
