2f911f956d894e39b211e8c3d367f3f56eefd636
SG-ThetZan <softguide.thetzan1@gmail.com>
2025-03-05 17:03:04 +0630

    ajax methods

--- static/js/api.js
+++ static/js/api.js
@@ -1,3 +1,64 @@
-const postRequest = (rul, request) => {
-    return $.ajax();
-}
+const postFormData = async (url, formData) => {
+    return await $.ajax({
+        url: url,
+        type: "POST",
+        contentType: false, 
+        processData: false, 
+        data: formData,
+    });
+};
+
+const postJson = async (url, request) => {
+    return await $.ajax({
+        url: url,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(request),
+        dataType: "json",
+    });
+};
+
+const updateFormData = async (url, idFromUi, formData) => {
+    const id = atob(idFromUi)
+    return await $.ajax({
+        url: url+id,
+        type: "POST",
+        contentType: false, 
+        processData: false, 
+        data: formData,
+    });
+};
+
+const updateJson = async (url, idFromUi, request) => {
+    const id = atob(idFromUi)
+    return await $.ajax({
+        url: url+id,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(request),
+        dataType: "json",
+    });
+};
+
+const getDataWithId = async (url, idFromUi) => {
+    const id = atob(idFromUi)
+    return await $.ajax({
+        url: url+id,
+        type: "GET",
+    });
+};
+
+const getAllData = async (url) => {
+    return await $.ajax({
+        url: url,
+        type: "GET",
+    });
+};
+
+const DeteleWithId = async (url, idFromUi) => {
+    const id = atob(idFromUi)
+    return await $.ajax({
+        url: url+id,
+        type: "DELETE",
+    });
+};
