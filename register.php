6adffc76faa2a0ae4fa050c899ef53b6d99b4285
SG-MiMiThant <softguide.mimithant96@gmail.com>
2025-03-08 11:40:49 +0630

    validate for email

--- register.php
+++ register.php
@@ -1,37 +1,38 @@
-<?php require_once ("./layout/header.php")?>
+<?php require_once("./layout/header.php") ?>
+
 <body>
-<div class="login-card">
+    <div class="login-card">
         <h1 class="header">Create Account</h1>
         <p class="sub-header">Please enter your details</p>
         <div class="form-group">
             <label for="name" class="label">Your name</label>
-            <input type="text" name="name" id="name" class="input">
-            <span class="validate"></span>
+            <input type="text" name="name" id="name" class="input" validate="blank|name">
+            <span class="validate" id="nameErr"></span>
         </div>
         <div class="form-group">
             <label for="email" class="label">Your email</label>
-            <input type="text" name="email" id="email" class="input">
-            <span class="validate"></span>
+            <input type="text" name="email" id="email" class="input" validate="blank|email">
+            <span class="validate" id="emailErr"></span>
         </div>
         <div class="form-group">
             <label for="password" class="label">Password</label>
             <div class="input-password">
-                <input type="password">
+                <input type="password" name="password" id="password" validate="blank|password">
                 <span><i class="fa-solid fa-eye-slash"></i></span>
             </div>
-            <span class="validate"></span>
+            <span class="validate" id="passwordErr"></span>
         </div>
         <div class="form-group">
-            <label for="confirm-password" class="label">Confirm password</label>
+            <label for="confirmPassword" class="label">Confirm password</label>
             <div class="input-password">
-                <input type="password">
+                <input type="confirmPassword" name="confirmPassword" id="confirmPassword" validate="blank|password">
                 <span><i class="fa-solid fa-eye-slash"></i></span>
             </div>
-            <span class="validate"></span>
+            <span class="validate" id="confirmPasswordErr"></span>
         </div>
-        <button class="login-btn">Register</button>
-        <p  class="account-login">Already have an account?<a href="#">Login here!</a></p>
+        <button id="register" class="login-btn">Register</button>
+        <p class="account-login">Already have an account?<a href="#">Login here!</a></p>
     </div>
-        
-<?php require_once ("./layout/footer.php")?>
-<script src="./static/js/reauestAPI/register.js"></script>
+
+    <?php require_once("./layout/footer.php") ?>
+    <script src="./static/js/reauestAPI/register.js"></script>
